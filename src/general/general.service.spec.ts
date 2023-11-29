31d00dc68da283a2ff387889526d16005587a549
a.marks <a.marks@conterra.de>
2023-11-29 11:22:50 +0100

    Example mock function

--- src/general/general.service.spec.ts
+++ src/general/general.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GeneralService } from './general.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { STANDARD_CRS } from './general.constants';
+import { Geometry } from 'typeorm';
 
 describe('GeneralService', () => {
   let service: GeneralService;
@@ -37,8 +38,27 @@ describe('GeneralService', () => {
     expect(service).toBeDefined();
   });
 
-  it('test crs', () => {
+  // example test specific function
+  it('should show correct crs', () => {
     const result = service.getCoordinateSystem({});
     expect(result).toBe(STANDARD_CRS);
+  });
+
+  // example mock function
+  // We can adjust the output of a service by mocking it. If this service is called by itself or another function
+  // our mocked version will always be executed (In this case it returns 1234, even the real version would return 4326)
+  it('should work with mock', () => {
+    const mockResult = 1234;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    jest
+      .spyOn(service, 'getCoordinateSystem')
+      .mockImplementation(({}) => mockResult);
+
+    const result = service.getCoordinateSystem({
+      type: 'Point',
+      coordinates: [0, 1],
+    } as Geometry);
+    expect(result).toBe(1234);
   });
 });
