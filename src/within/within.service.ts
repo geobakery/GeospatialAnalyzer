0b4fd60b0aac174a50f2e7b28f048023bfb979a1
a.marks <a.marks@conterra.de>
2023-11-29 16:33:40 +0100

    Remove Repository dependency from within completely

--- src/within/within.service.ts
+++ src/within/within.service.ts
@@ -3,9 +3,11 @@ import { GeneralService } from '../general/general.service';
 import { ParameterDto } from '../general/dto/parameter.dto';
 import { GeoJSON } from 'typeorm';
 import { dbRequestBuilderSample } from '../general/general.interface';
+import { ReplaceStringType } from '../general/general.constants';
 
-const WITHIN_WHERE_CLAUSE = 'ST_Within(:x::geometry, table1.geom)';
-const WITHIN_WHERE_CLAUSE_PARAMETER = 'x';
+const WITHIN_WHERE_CLAUSE =
+  "WHERE ST_Within('__a'::geometry, customFromSelect.geom)";
+const WITHIN_FROM = 'FROM __b customFromSelect';
 @Injectable()
 export class WithinService {
   constructor(private generalService: GeneralService) {}
@@ -14,13 +16,19 @@ export class WithinService {
   }
 
   async calculateWithin(args: ParameterDto): Promise<GeoJSON[]> {
-    return Promise.resolve([]);
-    // const dbBuilderParameter: dbRequestBuilderSample = {
-    //   select: false,
-    //   where: true,
-    //   whereStatement: WITHIN_WHERE_CLAUSE,
-    //   whereStatementParameter: WITHIN_WHERE_CLAUSE_PARAMETER,
-    // };
-    // return this.generalService.calculateMethode(args, dbBuilderParameter);
+    const dbBuilderParameter: dbRequestBuilderSample = {
+      select: false,
+      customStatement: true,
+      where: true,
+      whereStatement: WITHIN_WHERE_CLAUSE,
+      whereStatementParameter: new Map<string, ReplaceStringType>([
+        ['__a', ReplaceStringType.GEOMETRY],
+      ]),
+      fromStatement: WITHIN_FROM,
+      fromStatementParameter: new Map<string, ReplaceStringType>([
+        ['__b', ReplaceStringType.TABLE],
+      ]),
+    };
+    return this.generalService.calculateMethode(args, dbBuilderParameter);
   }
 }
