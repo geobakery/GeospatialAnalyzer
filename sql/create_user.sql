16e6567fdb7fd004e9d1d8337f379ce4b5171080
a.marks <a.marks@conterra.de>
2023-12-18 17:10:43 +0100

    update docker with complete database

---
+++ sql/create_user.sql
@@ -0,0 +1,2 @@
+CREATE EXTENSION postgis_raster;
+CREATE USER geobak_sp_admin SUPERUSER;
---
+++ sql/psql_old.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
+COPY spatialyzer_demo.verw_kreis_f (id, objectid, ifcid, nationalcode, id_localid, id_namespace, id_versionid, id_versionid_void, nationallevel, nationallevel_cl, nationallevelname_void, country, country_cl, residence_void, beginlifespanversion, beginlifespanversion_void, endlifespanversion, endlifespanversion_void, nuts_void, condominium_void, lowerlevelunit_void, upperlevelunit, upperlevelunit_void, admby_void, coadminister_void, boundary_void, name, geometrieflaeche, geometrieflaeche_eh, se_anno_cad_data, shape_area, shape_len, _predicate, geom) FROM '/docker-entrypoint-initdb.d/kreis.txt' DELIMITER ',';
+COPY spatialyzer_demo.adressen_p (id, objectid, rid, strasse, str_nummer, plz, ort, se_anno_cad_data, _predicate, geom) FROM '/docker-entrypoint-initdb.d/address.txt' DELIMITER ',';
+COPY spatialyzer_demo.verw_land_f (id, objectid, id00, name, nuts_code, egm_code, land, bundesland, geometrieflaeche, geometrieflaeche_eh, se_anno_cad_data, shape_area, shape_len, _predicate, geom) FROM '/docker-entrypoint-initdb.d/land.txt' DELIMITER ',';
+COPY spatialyzer_demo.verw_gem_f (id, objectid, ifcid, nationalcode, id_localid, id_namespace, id_versionid, id_versionid_void, nationallevel, nationallevel_cl, nationallevelname_void, country, country_cl, residence_void, beginlifespanversion, beginlifespanversion_void, endlifespanversion, endlifespanversion_void, nuts_void, condominium_void, lowerlevelunit_void, upperlevelunit, upperlevelunit_void, admby_void, coadminister_void, boundary_void, name, geometrieflaeche, geometrieflaeche_eh, se_anno_cad_data, shape_area, shape_len, _predicate, geom) FROM '/docker-entrypoint-initdb.d/gemeinde.txt' DELIMITER ',';
+EOSQL
