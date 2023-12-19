da639853de4b3b562edcf4b1e05782ad82fbf243
a.marks <a.marks@conterra.de>
2023-12-19 11:45:56 +0100

    use wkt parser

--- src/general/general.service.ts
+++ src/general/general.service.ts
@@ -31,6 +31,7 @@ import {
 import { InjectDataSource } from '@nestjs/typeorm';
 import { ParameterDto } from './dto/parameter.dto';
 import { ConfigService } from '@nestjs/config';
+import { geojsonToWKT } from '@terraformer/wkt';
 
 @Injectable()
 export class GeneralService {
@@ -167,79 +168,9 @@ export class GeneralService {
    */
   _buildGeometry(geo: Geometry, crs: number): string {
     let result = 'SRID=' + crs + ';';
-    const geoType = geojsonToPostGis.get(geo.type);
-    result += geoType;
-    const coordinates = this._buildCoordinatesFromDBType(geo);
-    result += '(' + coordinates + ')';
+    const coordinates = geojsonToWKT(geo);
+    result += coordinates;
     return result;
-  }
-
-  /**
-   * Explanation:
-   * helper function to transform geojson types to coordinate strings
-   */
-  _buildCoordinatesFromDBType(geo: Geometry): string {
-    switch (geo.type) {
-      case 'Point': {
-        const point = geo as Point;
-        return '' + point.coordinates[0] + ' ' + point.coordinates[1];
-      }
-      case 'GeometryCollection':
-      case 'MultiPolygon':
-      case 'MultiLineString':
-      case 'MultiPoint': {
-        //TODO
-        // Example GEOMETRYCOLLECTION(POINT(2 0),POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))');
-        switch (geo.type) {
-          case 'MultiLineString': {
-            break;
-          }
-          case 'MultiPoint': {
-            break;
-          }
-          case 'GeometryCollection': {
-            break;
-          }
-          case 'MultiPolygon': {
-            break;
-          }
-        }
-        return '';
-      }
-      case 'LineString': {
-        const line = geo as LineString;
-        let result: string = '';
-        line.coordinates.forEach((c) => {
-          result += '' + c.join(' ') + ',';
-        });
-        if (result.length) {
-          result = result.slice(0, -1);
-        }
-        return result;
-      }
-      case 'Polygon': {
-        const polygon = geo as Polygon;
-        let result: string = '';
-        polygon.coordinates.forEach((c) => {
-          result += '(';
-          c.forEach((c2) => {
-            result += '' + c2.join(' ') + ',';
-          });
-          if (result.length) {
-            // remove last comma for segment
-            result = result.slice(0, -1);
-          }
-          result += '),';
-        });
-
-        if (result.length) {
-          // remove last comma for polygon
-          result = result.slice(0, -1);
-        }
-        return result;
-      }
-    }
-    return '';
   }
 
   /*
