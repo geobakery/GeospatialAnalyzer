c6b9a99345c864be8a5953cbac32b00389b8fa0b
d.schlarmann <d.schlarmann@conterra.de>
2024-01-12 16:18:05 +0100

    Updated Health-Interface by adding actual database query for ensuring database-availability

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { GeneralService } from '../general/general.service';
+import {
+  dbRequestBuilderSample,
+  ErrorResponse,
+  GeneralResponse,
+} from '../general/general.interface';
 
+const HEALTH_CALL = 'SELECT 1';
 @Injectable()
 export class HealthService {
-  getHealth(): any {
-    const healthcheck = {
-      api: 'ok',
-      timestamp: Date.now(),
+  constructor(private generalService: GeneralService) {}
+
+  async getCurrentHealth(): Promise<GeneralResponse | ErrorResponse> {
+    const dbBuilderParameter: dbRequestBuilderSample = {
+      select: false,
+      customStatement: true,
+      where: false,
+      selectStatement: HEALTH_CALL,
     };
-    return healthcheck;
+    const databaseResult =
+      await this.generalService.executePlainDatabaseQuery(dbBuilderParameter);
+    if (databaseResult) {
+      return { response: 'ok' } as GeneralResponse;
+    } else {
+      return {
+        status: 500,
+        error: 'Fail - Database is not available.',
+      } as ErrorResponse;
+    }
   }
 }
