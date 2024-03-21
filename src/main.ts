240a43f0bb16f4f207b04d4591e27e72a4ab1792
Henner Niehaus <h.niehaus@conterra.de>
2024-03-21 09:52:24 +0100

    Move OpenAPI description into dedicated file

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import * as fs from 'fs';
-import { join } from 'path';
-import { setUpOpenAPIAndValidation } from './app-init';
+import { checkTopic, setUpOpenAPIAndValidation } from './app-init';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -28,31 +26,4 @@ async function bootstrap() {
   await app.listen(3000, '0.0.0.0');
 }
 
-async function checkTopic(): Promise<void> {
-  const fileExists = async (path: string) =>
-    !!(await fs.promises.stat(path).catch(() => false));
-
-  const exist = await fileExists(join(__dirname, './../topic.json'));
-  if (!exist) {
-    console.warn(
-      'No topic.json defined. Please read the README (chapter configuration) and add the specific configuration file. A default one will be added.',
-    );
-    await copyTopic(__dirname);
-  }
-  return;
-}
-
-async function copyTopic(dirPath: string): Promise<void> {
-  fs.copyFile(
-    join(dirPath, './../topic-example-geosn.json'),
-    join(dirPath, './../topic.json'),
-    (err) => {
-      if (err) {
-        console.log('Error Found:', err);
-        process.exit(1);
-      }
-    },
-  );
-}
-
-bootstrap();
+void bootstrap();
