b923208c231991b7e2ef5b6bec07a3fba77ca9cd
Tim Moser <t.moser@conterra.de>
2026-02-27 13:53:47 +0100

    feat(openapi): add env option to hide metrics endpoint
    - True by default on production environments.
    - Should be used in addition to securing the endpoint directly, i.e. preventing external access to this endpoint. See https://github.com/geobakery/gsa-deployment/commit/aab4b020ddf871345e1cb89169b0ec7f2d43a412. Otherwise, the endpoint is only hidden in the swagger api documentation and still accessible.

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,11 +1,23 @@
 import { Controller, Get, Header } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags, ApiExcludeController } from '@nestjs/swagger';
 import { MetricsService } from './metrics.service';
+import * as dotenv from 'dotenv';
+
+// we have to load this env var without using nestjs config service, because we need the value to define the module decorators
+function preloadSpecificEnvVar(varName: string): string | undefined {
+  const tempEnvStore : dotenv.DotenvPopulateInput = {} // loading in temp object to avoid any side-effects
+  dotenv.config({path: ['.env.dev', '.env'], processEnv: tempEnvStore, quiet: true});
+  return tempEnvStore[varName];
+}
+
+const GEOSPATIAL_ANALYZER_METRICS_ENDPOINT_HIDDEN = preloadSpecificEnvVar('GEOSPATIAL_ANALYZER_METRICS_ENDPOINT_HIDDEN');
+const EXCLUDE_CONTROLLER_FROM_API_DOCS = GEOSPATIAL_ANALYZER_METRICS_ENDPOINT_HIDDEN ? JSON.parse(GEOSPATIAL_ANALYZER_METRICS_ENDPOINT_HIDDEN) : process.env.NODE_ENV === 'production';
 
 /**
  * Controller for exposing Prometheus metrics endpoint
  */
 @ApiTags('Metrics')
+@ApiExcludeController(EXCLUDE_CONTROLLER_FROM_API_DOCS)
 @Controller('metrics')
 export class MetricsController {
   constructor(private readonly metricsService: MetricsService) {}
